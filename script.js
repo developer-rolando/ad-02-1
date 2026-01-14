e46e1471e225b55b02df6bfa581f677f18bfc7e3
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2026-01-14 13:17:22 -0600

    Cambiando color de encabezado al clikear a cafe

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const h1Element = document.getElementById("title");
 const h4Element = document.getElementById("h4Id");
+const h2Element = document.getElementById("red");
 
 
 h1Element.textContent = "Adios!";
 h4Element.style.color = "Orange";
+
+h2Element.addEventListener("click", () =>{
+    h2Element.style.color = "brown";
+});
